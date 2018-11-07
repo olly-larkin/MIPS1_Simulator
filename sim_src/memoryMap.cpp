efc17e1ade44808dbe92ac7da4062622a38b3491
Olly Larkin <ollylarkin@ollys-mbp.home>
2018-11-07 13:22:13 +0000

    memory made efficient

--- sim_src/memoryMap.cpp
+++ sim_src/memoryMap.cpp
@@ -2,12 +2,8 @@
 #include "memoryMap.hpp"
 #include <fstream>
 
-MemoryMap::MemoryMap() {
-    ADDR_NULL = std::vector<char>(ADDR_NULL_SIZE, 0);
-    ADDR_INSTR = std::vector<char>(ADDR_INSTR_SIZE, 0);
-    ADDR_DATA = std::vector<char>(ADDR_DATA_SIZE, 0);
-    ADDR_GETC = std::vector<char>(ADDR_GETC_SIZE, 0);
-    ADDR_PUTC = std::vector<char>(ADDR_PUTC_SIZE, 0);
+MemoryMap::~MemoryMap() {
+    delete[] ADDR_INSTR;
 }
 
 int32_t MemoryMap::read(unsigned int addr, unsigned char byteNum, bool signedRead) {
@@ -19,19 +15,30 @@ int32_t MemoryMap::read(unsigned int addr, unsigned char byteNum, bool signedRea
     int32_t returnVal = 0;
     for(int i = 0; i < byteNum; ++i) {
         int shift = (byteNum - i - 1) * 8;
-        if (addr < ADDR_NULL_SIZE) {
+        if (addr < ADDR_NULL_SIZE) {        // NULL
+
             //Nothing should happen
-        } else if (addr >= ADDR_INSTR_P && addr < ADDR_INSTR_P + ADDR_INSTR_SIZE) {
-            returnVal += ((ADDR_INSTR[addr - ADDR_INSTR_P] & 0xFF) << shift);
-        } else if (addr >= ADDR_DATA_P && addr < ADDR_DATA_P + ADDR_DATA_SIZE) {       
-            returnVal += ((ADDR_DATA[addr - ADDR_DATA_P] & 0xFF) << shift);
-        } else if (addr >= ADDR_GETC_P && addr < ADDR_GETC_P + ADDR_GETC_SIZE) {
+
+        } else if (addr >= ADDR_INSTR_P && addr < ADDR_INSTR_P + ADDR_INSTR_SIZE) {     // ADDR_INSTR
+            
+            if (addr <= instrSize + ADDR_INSTR_P + 1){
+                returnVal += ((ADDR_INSTR[addr - ADDR_INSTR_P] & 0xFF) << shift);
+            }
+
+        } else if (addr >= ADDR_DATA_P && addr < ADDR_DATA_P + ADDR_DATA_SIZE) {        // ADDR_DATA
+
+            if (ADDR_DATA.find(addr - ADDR_DATA_P) != ADDR_DATA.end())
+                returnVal += ((ADDR_DATA[addr - ADDR_DATA_P] & 0xFF) << shift);
+
+        } else if (addr >= ADDR_GETC_P && addr < ADDR_GETC_P + ADDR_GETC_SIZE) {        // ADDR_GETC
+
             if (addr == ADDR_GETC_P + 3) {
                 int32_t inputVal = getchar();
                 if (inputVal == EOF)
                     return -1;
                 returnVal += inputVal;
             }
+
         } else {
             std::cerr << "Invalid memory read." << std::endl << std::endl;
             std::exit(-11);
@@ -57,7 +64,9 @@ void MemoryMap::write(unsigned int addr, int32_t data, unsigned char byteNum) {
     for(int i = 0; i < byteNum; ++i) {
         int shift = (byteNum - i - 1) * 8;
         if (addr >= ADDR_DATA_P && addr < ADDR_DATA_P + ADDR_DATA_SIZE) {
+
             ADDR_DATA[addr - ADDR_DATA_P] = ((data >> shift) & 0xFF);
+
         } else if (addr >= ADDR_PUTC_P && addr < ADDR_PUTC_P + ADDR_PUTC_SIZE) {
             output = true;
             if (addr == ADDR_PUTC_P + 3)
@@ -76,10 +85,13 @@ void MemoryMap::write(unsigned int addr, int32_t data, unsigned char byteNum) {
 void MemoryMap::instrDump(std::ifstream& binFile) {
     binFile.seekg(0, std::ios::end);
     std::streampos size = binFile.tellg();
+    instrSize = (int)size;
     if (size > ADDR_INSTR_SIZE) {
         std::cerr << ".bin file too large." << std::endl << std::endl;
         std::exit(-21);
     }
+    ADDR_INSTR = new char[instrSize+1];
+    ADDR_INSTR[instrSize] = 0;
     binFile.seekg(0, std::ios::beg);
     binFile.read(&ADDR_INSTR[0], size);
     binFile.close();
